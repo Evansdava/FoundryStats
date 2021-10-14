c49d044135b90f5f29c8a26745d1a2d179c4b24b
David Evans <evansdav.a@gmail.com>
2021-10-14 16:12:20 -0700

    Bot listens and prints received messages

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const bot = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"]});
 const TOKEN = process.env.TOKEN;
 
 bot.login(TOKEN);
 
 bot.on('ready', () => {
-  console.info(`Logged in as ${bot.user.tag}!`);
+    console.info(`Logged in as ${bot.user.tag}!`);
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
-
-  }
+    console.log(msg.embeds[0].description)
 });
